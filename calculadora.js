fab164ac4a6e074560d3c16bf3499a0730812329
Bruno Moretto <bmoretto404@gmail.com>
2023-08-17 16:24:28 -0300

    Development

--- calculadora.js
+++ calculadora.js
@@ -1,4 +1,171 @@
-const add = (num1, num2) => num1 + num2;
-const sub = (num1, num2) => num1 - num2;
-const div = (num1, num2) => num1 / num2;
-const mult = (num1, num2) => num1 * num2;
+let stack = [0];
+let isResult = false;
+let lastOp = '';
+
+const highPriorityOperationSigns = ['*', '/'];
+const lowPriorityOperationSigns = ['+', '-'];
+const operationSigns = highPriorityOperationSigns + lowPriorityOperationSigns;
+
+const cleanStack = () => {
+    stack = [0];
+    updateDisplayNumber(0);
+    removeActionButtonClass(lastOp);
+}
+const isAnOperationSign = value => operationSigns.includes(value);
+const isFloatNumber = value => value.toString().includes('.');
+const updateDisplayNumber = number => document.getElementById('number-display').innerHTML = number;
+const setActionButtonClass = operation => {
+    switch (operation) {
+        case '+':
+            document.getElementById('add-op').classList.add('action-button-active');
+            break;
+        case '-':
+            document.getElementById('sub-op').classList.add('action-button-active');
+            break;
+        case '*':
+            document.getElementById('mult-op').classList.add('action-button-active');
+            break;
+        case '/':
+            document.getElementById('div-op').classList.add('action-button-active');
+            break;
+        default:
+            return;
+    }
+}
+
+const removeActionButtonClass = operation => {
+    switch (operation) {
+        case '+':
+            document.getElementById('add-op').classList.remove('action-button-active');
+            break;
+        case '-':
+            document.getElementById('sub-op').classList.remove('action-button-active');
+            break;
+        case '*':
+            document.getElementById('mult-op').classList.remove('action-button-active');
+            break;
+        case '/':
+            document.getElementById('div-op').classList.remove('action-button-active');
+            break;
+        default:
+            '';
+    }
+}
+
+const pushOp = value => {
+    isResult = false;
+    removeActionButtonClass(lastOp);
+    
+    const lastStackIndex = stack.length-1
+    const lastStackValue = stack[lastStackIndex];
+
+    if (isAnOperationSign(lastStackValue)) {
+        stack.splice(lastStackIndex, 1, value);
+    } else {
+        if (highPriorityOperationSigns.includes(lastOp)) {
+            result();
+            isResult = false;
+        }
+
+        stack.push(value);
+    }
+
+    setActionButtonClass(value);
+    lastOp = value;
+};
+
+const pushNumber = value => {
+    const lastStackIndex = stack.length-1;
+    const lastStackValue = stack[lastStackIndex];
+
+    if (isResult) {
+        isResult = false;
+        stack = [0];
+    }
+
+    if (!isFloatNumber(stack[0]) && stack.length === 1 || isAnOperationSign(lastStackValue)) {
+        stack.push(value); 
+    } else {
+        value = lastStackValue + '' + value;
+        stack.splice(lastStackIndex, 1, value);
+    }
+
+    updateDisplayNumber(value);
+    removeActionButtonClass(lastOp);
+}
+
+const pushPoint = () => {
+    const lastStackIndex = stack.length-1;
+    const lastStackValue = stack[lastStackIndex];
+
+    if (isFloatNumber(lastStackValue)) {
+        return;
+    }
+
+    isResult = false;
+
+    if (isAnOperationSign(lastStackValue)) {
+        value = 0 + '.'
+        stack.push(value);
+    } else {
+        value = lastStackValue + '.';
+        stack.splice(lastStackIndex, 1, value);
+    }
+
+    updateDisplayNumber(value);
+}
+
+const pushPercent = () => {
+    isResult = false;
+    const lastStackIndex = stack.length-1;
+    const lastStackValue = stack[lastStackIndex];
+
+    if (isAnOperationSign(lastStackValue)) {
+        return;
+    }
+
+    const result = lastStackValue/100;
+    stack.splice(lastStackIndex, 1, result);
+    updateDisplayNumber(result);
+}
+
+const pushSignal = () => {
+    isResult = false;
+    const lastStackIndex = stack.length-1;
+    const lastStackValue = stack[lastStackIndex];
+
+    if (isAnOperationSign(lastStackValue)) {
+        return;
+    }
+
+    const result = -lastStackValue;
+    stack.splice(lastStackIndex, 1, result);
+    updateDisplayNumber(result);
+}
+
+// TODO tratar grandes números
+const calculate = () => {
+    let op = '';
+    stack.forEach(value => {
+        if (value < 0 && op.endsWith('-')) {
+            value = '('+value+')';
+        }
+
+        op = op.concat(value)
+    });
+
+    return eval(op);
+}
+
+const result = () => {
+    if (stack[0] == 0 && !isAnOperationSign(stack[1])) {
+        stack.splice(0, 1);
+    }
+
+    const result = calculate();
+    stack = [result];
+    isResult = true;
+    removeActionButtonClass(lastOp);
+    lastOp = '';
+    updateDisplayNumber(result);
+}
